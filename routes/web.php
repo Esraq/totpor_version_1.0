c656befc651cfb3d8693757dfd549ff2d30de966
esraq <esraq26552@gmail.com>
2025-01-22 00:37:31 +0600

    Add example.txt file

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\RegistrationController;
+
+
+use App\Http\Controllers\RegisteredController;
+
+use App\Http\Controllers\SiteController;
+
+
+
+
+
+Route::get('/', [SiteController::class, 'site'])->name('/');
+
+Route::get('/how_it_works', [SiteController::class, 'about'])->name('/how_it_works');
+
+
+Route::get('/categories', [SiteController::class, 'category'])->name('/categories');
+
+
+
+Route::get('/tasks', [SiteController::class, 'tasks'])->name('/tasks');
+
+
+
+Route::get('/signup', [SiteController::class, 'signup'])->name('/signup');
